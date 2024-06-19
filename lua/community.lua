9ee2ae9c0a0c604681e6cb290123c1e9b1b81045
Rob Patro <rob.patro@gmail.com>
2024-06-18 22:51:51 -0400

    first commit

---
+++ lua/community.lua
@@ -0,0 +1,18 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.motion.marks-nvim" },
+  -- import/override with your plugins folder
+
+  -- color schemes
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+}
